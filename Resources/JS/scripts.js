6a8d1f2d466f05f1a1d2ef0dadbe89d49db32c73
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-07-10 13:52:39 -0700

    finished Omnifood website

--- Resources/JS/scripts.js
+++ Resources/JS/scripts.js
@@ -1,3 +1,105 @@
 $(document).ready(function() {
+    // For the waypoint trigger to employ the sticky navigation. When scrolling down, the sticky class is added (http://imakewebthings.com/waypoints/, downloaded and the minified version placed in vendors folder, JS)
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+
+    // Scroll on buttons in header
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
+    });
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top},500);
+    });
+
+    // Navigation scroll- scrolls to sections from the nav bar (from https://css-tricks.com/snippets/jquery/smooth-scrolling/, copied)
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+    // On-page links
+        if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+        ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+            scrollTop: target.offset().top
+            }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+            } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+            };
+            });
+        }
+        }
+    });
     
-})
+    // animations on scroll, from waypoints (reference above) and animate.css (https://daneden.github.io/animate.css/)
+
+    // animates a fade in for our about content
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    // fade in phone
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    // fade in cities
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    // pulsing high tier food plan
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+
+});
+
+// Mobile navicationg
+
+// Toggle goes down then up for some reason, not changing class to the other icon
+$('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+    // 200ms
+    nav.slideToggle(200);
+    
+    if (icon.hasClass('ion-navicon-round')) {
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+    } else {
+        icon.removeClass('ion-close-round');
+        icon.addClass('ion-navicon-round');
+    }
+
+});
